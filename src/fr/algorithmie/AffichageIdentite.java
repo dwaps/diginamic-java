93d19c24cea884fa70313639309f0fe64e889212
michael <ei.dwaps@gmail.com>
2024-04-30 09:59:38 +0200

    exercice affichage identité

---
+++ src/fr/algorithmie/AffichageIdentite.java
@@ -0,0 +1,11 @@
+package fr.algorithmie;
+
+public class AffichageIdentite {
+  public static void main(String[] args) {
+    String identity = "Michael CORNILLON";
+
+    for (int i = 0; i < 10; i++) {
+      System.out.println(identity);
+    }
+  }
+}
