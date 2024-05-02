520cf7d4213a2a43a22f732f81e390718737b3e1
michael <ei.dwaps@gmail.com>
2024-05-02 11:09:04 +0200

    ajout accesseurs / mutateurs

--- src/fr/diginamic/entities/AdressePostale.java
+++ src/fr/diginamic/entities/AdressePostale.java
@@ -1,10 +1,42 @@
 package fr.diginamic.entities;
 
 public class AdressePostale {
-  int numbRue;
-  String rue;
-  String codePostal;
-  String ville;
+  private int numbRue;
+  private String rue;
+  private String codePostal;
+  private String ville;
+
+  public int getNumbRue() {
+    return numbRue;
+  }
+
+  public void setNumbRue(int numbRue) {
+    this.numbRue = numbRue;
+  }
+
+  public String getRue() {
+    return rue;
+  }
+
+  public void setRue(String rue) {
+    this.rue = rue;
+  }
+
+  public String getCodePostal() {
+    return codePostal;
+  }
+
+  public void setCodePostal(String codePostal) {
+    this.codePostal = codePostal;
+  }
+
+  public String getVille() {
+    return ville;
+  }
+
+  public void setVille(String ville) {
+    this.ville = ville;
+  }
 
   public AdressePostale(int numbRue, String rue, String codePostal, String ville) {
     this.numbRue = numbRue;
--- src/fr/diginamic/entities/Personne.java
+++ src/fr/diginamic/entities/Personne.java
@@ -1,9 +1,34 @@
 package fr.diginamic.entities;
 
 public class Personne {
-  String nom;
-  String prenom;
-  AdressePostale adresse;
+  private String nom;
+  private String prenom;
+
+  public String getNom() {
+    return nom;
+  }
+
+  public void setNom(String nom) {
+    this.nom = nom;
+  }
+
+  public String getPrenom() {
+    return prenom;
+  }
+
+  public void setPrenom(String prenom) {
+    this.prenom = prenom;
+  }
+
+  public AdressePostale getAdresse() {
+    return adresse;
+  }
+
+  public void setAdresse(AdressePostale adresse) {
+    this.adresse = adresse;
+  }
+
+  private AdressePostale adresse;
 
   public Personne(String nom, String prenom, AdressePostale adresse) {
     this.nom = nom;
