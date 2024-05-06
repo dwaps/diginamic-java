9e2e56710a7f3cea4cad9f84f76ba7ad692b56a5
michael <ei.dwaps@gmail.com>
2024-05-06 15:55:45 +0200

    init

---
+++ src/main/java/fr/diginamic/openfoodfacts/App.java
@@ -0,0 +1,13 @@
+package fr.diginamic.openfoodfacts;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
